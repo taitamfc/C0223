e2e7460490f9b467066e86483d77e75c96d15831
taitamfc <taitam8590@gmail.com>
2023-04-06 10:43:46 +0700

    form.php

---
+++ M2/form.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+
+<form action="" method="">
+    username: <input type="text" name="username"> <br>
+    email: <input type="email" name="email"> <br>
+    password: <input type="password" name="password"> <br>
+    <input type="submit" value="Gui">
+</form>
