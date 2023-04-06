d9f85fc3434ef2f6008782d5d64c329158dbe2c7
taitamfc <taitam8590@gmail.com>
2023-04-06 10:58:58 +0700

    form.php server

--- M2/form.php
+++ M2/form.php
@@ -1,11 +1,16 @@
 <?php
 
-    echo '<pre>';
-    print_r( $_POST );
-    echo '</pre>';
+    
+
+    //Kiem tra nguoi dung da gui du lieu len
+    if( $_SERVER['REQUEST_METHOD'] == 'POST'){
+        echo '<pre>';
+        print_r( $_REQUEST );
+        echo '</pre>';
+    }
 
 ?>
-<form action="" method="POST">
+<form action="" method="post">
     username: <input type="text" name="username"> <br>
     email: <input type="email" name="email"> <br>
     password: <input type="password" name="password"> <br>
