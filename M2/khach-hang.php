49b2154cf89e5518ed13046f9386785d92959297
taitamfc <taitam8590@gmail.com>
2023-04-06 11:24:37 +0700

    array

---
+++ M2/khach-hang.php
@@ -0,0 +1,15 @@
+<?php
+    $tusach = [
+        'mot' => [
+            'ten' => 'Van Hoc',
+            'gia' => 12000
+        ],
+        'hai' => [
+            'ten' => 'Su Hoc',
+            'gia' => 12000
+        ]
+    ];
+
+    // echo '<pre>';
+    print_r($tusach);
+    // echo '</pre>';
