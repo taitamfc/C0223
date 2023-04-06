6a54f0e2b1773157955a343e153029877d966af6
taitamfc <taitam8590@gmail.com>
2023-04-06 11:06:43 +0700

    echo bien

--- M2/form.php
+++ M2/form.php
@@ -1,12 +1,26 @@
 <?php
 
-    
+    // $books = ['Van','Su','Dia'];
+    // //          0     1    2
+
+    // echo $books[0];
+
+    // $books = [
+    //     'khong' => 'Van',
+    //     'mot' => 'Su',
+    //     'hai' => 'Dia',
+    // ];
+
+    // echo $books['khong'];
 
     //Kiem tra nguoi dung da gui du lieu len
     if( $_SERVER['REQUEST_METHOD'] == 'POST'){
         echo '<pre>';
         print_r( $_REQUEST );
         echo '</pre>';
+        echo '<br>'. $_REQUEST['username'];
+        echo '<br>'. $_REQUEST['email'];
+        echo '<br>'. $_REQUEST['password'];
     }
 
 ?>
