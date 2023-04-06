32686df7b5489ccd2b679d1ee5cf7b95abdd9ce7
taitamfc <taitam8590@gmail.com>
2023-04-06 10:47:33 +0700

    form.php print_R

--- M2/form.php
+++ M2/form.php
@@ -1,9 +1,11 @@
 <?php
 
+    echo '<pre>';
+    print_r( $_POST );
+    echo '</pre>';
+
 ?>
-
-
-<form action="" method="">
+<form action="" method="POST">
     username: <input type="text" name="username"> <br>
     email: <input type="email" name="email"> <br>
     password: <input type="password" name="password"> <br>
