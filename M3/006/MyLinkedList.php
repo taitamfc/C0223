c07b740c42bdd70326700247bb0f2d684c643e47
taitamfc <taitam8590@gmail.com>
2023-05-16 10:04:55 +0700

    006

--- M3/006/MyLinkedList.php
+++ M3/006/MyLinkedList.php
@@ -2,12 +2,17 @@
 // Khai bao lop Node
 class Node{
     public $data;
+    public $score;
     public $next;
 
-    public function __construct($data){
+    public function __construct($data,$score = 0){
         $this->data = $data;
+        $this->score = $score;
         $this->next = NULL;
     }
+
+
+
 }
 // Khai bao lop LinkedList
 class LinkedList{
@@ -15,8 +20,13 @@ class LinkedList{
     public function __construct(){
         $this->firstNode = null;
     }
-    public function add($item){
-        $node = new Node($item);
+    public function addFirst($item,$score = 0){
+        $node = new Node($item,$score);
+        $node->next = $this->firstNode;
+        $this->firstNode = $node;
+    }
+    public function addLast($item,$score = 0){
+        $node = new Node($item,$score);
         if( $this->firstNode == null ){
              /*
             Node Object
@@ -49,18 +59,59 @@ class LinkedList{
             */
             
         }
-       
-        
+    }
+
+    public function showList(){
+        $current = $this->firstNode;
+        while ($current != null) {
+            echo $current->data.'-'.$current->score.'<br>';
+            $current = $current->next;
+        }
+    }
+    public function totalStudentsFail(){
+        $current = $this->firstNode;
+        $count = 0;
+        while ($current != null) {
+            if( $current->score < 3 ){
+                $count++;
+            }
+            $current = $current->next;
+        }
+        return $count;
+    }
+    public function listStudentMaxScore(){
+        $current = $this->firstNode;
+        $students = [];
+        while ($current != null) {
+            if( $current->score >= 3 ){
+                $students[] = $current;
+            }
+            $current = $current->next;
+        }
+        return $students;
+    }
+
+    public function findByName($name){
+        $current = $this->firstNode;
+        while ($current != null) {
+            if( $current->data == $name ){
+                return $current;
+            }
+            $current = $current->next;
+        }
+        return false;
     }
 }
 
 $objLinkedList = new LinkedList();
-$objLinkedList->add('Khuong');
-$objLinkedList->add('Hieu');
-$objLinkedList->add('Phi');
-$objLinkedList->add('Long');
+$objLinkedList->addLast('Khuong',1);
+$objLinkedList->addLast('Hieu',2);
+$objLinkedList->addLast('Phi',4);
+$objLinkedList->addLast('Long',3);
+$objLinkedList->addFirst('Tam',5);
 
 echo '<pre>';
+$objLinkedList->showList();
 print_r($objLinkedList);
 die();
 
