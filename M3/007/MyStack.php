f5b7efb1efbf74b69086472b290509312eaf6589
taitamfc <taitam8590@gmail.com>
2023-05-17 10:19:01 +0700

    MyStack

--- M3/007/MyStack.php
+++ M3/007/MyStack.php
@@ -8,41 +8,59 @@ class MyStack {
     }
     //dua phan tu vao danh sach
     public function push($item){
-        
+        array_unshift($this->elements,$item);
     }
 
     // lay phan tu ra khoi ngan xep
     public function pop(){
-        
+        array_shift($this->elements);   
     }
 
     // xem phan tu dau tien cua ngan xep
     public function peek(){
-        
+        return current($this->elements);
     }
 
     // kiem tra rong
     public function isEmpty(){
-       
+    //    return empty($this->elements);
+        if(count($this->elements)==0){
+            return true;
+        }
+        return false;
     }
     // xem danh sach co gi
     public function getStack(){
-       
+       return $this->elements;
     }
     // kiem tra day
     public function isFull(){
-        
+        if(count($this->elements) == $this->limit){
+            return true;
+        }
+        return false;
     }
 }
 // KHoi tao doi tuong
 $mystack = new MyStack(4);
 // Them vao lan luot Huyen, Nho, Phong, Tam
-
+$mystack->push('huyen');
+$mystack->push('nho');
+$mystack->push('phong');
+$mystack->push('tam');
 // Lay ra phan tu tren cung ma ko xoa
+echo $mystack->peek();
 // Lay ra phan tu tren cung va xoa di
+$mystack->pop();
+
 // Xem danh sach co rong khong ?
+var_dump($mystack->isEmpty());
 // Xem danh sach co day khong
+var_dump($mystack->isFull());
 // Xem toan bo cac phan tu trong danh sach
+echo "<pre>";
+print_r($mystack->getStack());
+echo "</pre>";
 
 // In doi tuong
 echo "<pre>";
