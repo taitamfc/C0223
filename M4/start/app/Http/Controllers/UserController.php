3a899600d010b545cdeffcbdaab0c52f3128334a
taitamfc <taitam8590@gmail.com>
2023-06-05 09:02:23 +0700

    Init project

---
+++ M4/start/app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; //$_REQUEST
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        // $items = User::all();
+        $items = [];
+        return view('users.index', compact(['items']));
+    }
+    public function store(Request $request){
+        // echo '<pre>';
+        // print_r($_REQUEST);
+        // echo '</pre>';
+
+
+        // $username = $_REQUEST['username'];
+        // $password = $_REQUEST['password'];
+        
+        $username = $request->username;
+        $password = $request->password;
+
+
+    }
+}
---
+++ M4/start/resources/views/users/index.blade.php
@@ -0,0 +1,8 @@
+<h1>Khuong's Page</h1>
+
+<form method="post" action="/xulyKhuong">
+    @csrf
+    username: <input type="text" name="username" id=""> <br>
+    password: <input type="password" name="password" id=""> <br>
+    <input type="submit" value="Login">
+</form>
