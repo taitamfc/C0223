6036ab07719f6a624cb12bfda7f56d19c33db71e
taitamfc <taitam8590@gmail.com>
2023-06-21 09:53:12 +0700

    api

--- M4/start/routes/api.php
+++ M4/start/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('users',UserController::class);
+
+Route::post('/login',[UserController::class,'login']);
+Route::post('/register',[UserController::class,'register']);
