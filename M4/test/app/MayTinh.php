bf1962f26434909b913043aac04bb3b6454c2823
taitamfc <taitam8590@gmail.com>
2023-06-30 08:35:05 +0700

    test

---
+++ M4/test/app/MayTinh.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+class MayTinh {
+    public function cong($a,$b){
+        
+    }
+    public function tru($a,$b){
+        
+    }
+    public function nhan($a,$b){
+        
+    }
+    public function chia($a,$b){
+        
+    }
+}
