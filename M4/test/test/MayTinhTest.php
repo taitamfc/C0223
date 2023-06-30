f64aa8168c8c0ce0895231a153d15fa65783d8eb
taitamfc <taitam8590@gmail.com>
2023-06-30 08:46:43 +0700

    1. Viet test case

--- M4/test/test/MayTinhTest.php
+++ M4/test/test/MayTinhTest.php
@@ -3,5 +3,15 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 use App\MayTinh;
 class MayTinhTest extends TestCase {
-    
+    public function test_cong_case_1(){
+        $objMayTinh = new MayTinh();
+        $a = 2;
+        $b = 3;
+
+        $expected_output    = 5;
+        $your_output        = $objMayTinh->sum($a,$b);
+
+        // So sanh bang
+        $this->assertEquals($your_output,$expected_output);
+    }
 }
