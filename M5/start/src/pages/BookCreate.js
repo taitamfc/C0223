5f8608abf51d5e05bd5f33e5323270fd8abe41ff
taifamfc <taitam8590@gmail.com>
2023-07-24 08:56:52 -0700

    BookCreate FOrm

--- M5/start/src/pages/BookCreate.js
+++ M5/start/src/pages/BookCreate.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import { Field, Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("Required"),
+    price: Yup.string()
+      .required("Required")
+  });
 
 function BookCreate(props) {
+    const [form,setForm] = useState({
+        name: '',
+        price: 0
+    });
+
+    const handleSubmit = (data) => {
+
+    }
+
     return (
         <div>
             <h1>BookCreate</h1>
-            
+            <Formik
+                initialValues={form}
+                validationSchema={SignupSchema}
+                onSubmit={(values) => handleSubmit(values)}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+                        <label htmlFor="name">First Name</label>
+                        <Field name="name" />
+                        {errors.name && touched.name ? (
+                            <div>{errors.name}</div>
+                        ) : null}
+                        <label htmlFor="price">Price</label>
+
+                        <Field name="price" />
+                        {errors.price && touched.price ? (
+                            <div>{errors.price}</div>
+                        ) : null}
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 }
